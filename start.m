0ea66d10d6594f7afda59eeba93b8ca83eeccd7b
Mohamed <elraghy8@gmail.com>
2019-05-10 01:45:35 +0200

    calculate The Accuracy

--- start.m
+++ start.m
@@ -3,12 +3,18 @@ clear ; close all; clc
 %% Setup the NN 
 load('iris.mat');
 load('test.mat');
+load('testLabel.mat');
 net = iris;
 %% Training 
-train(net, input, output);
+net = train(net, input, output);
 %% show Weights
 [inputWeight, layerWegiht] = showWeights(net)
 fprintf('Input Wegiht Matrix\n');
 inputWeight
 fprintf('layer Wegiht Matrix\n');
 layerWegiht
+clear inputWeight layerWegiht;
+%% calc Accuracy
+pred = sim(net, test);
+acc = mean(round(pred) == testLabel);
+fprintf('Accuracy = %f\n', acc * 100);
